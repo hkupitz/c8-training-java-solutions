a979e9de635efb3ff1d55b3b30d46364bd8b7fbc
Hendrik Kupitz <hendrik.kupitz@camunda.com>
2024-04-17 10:03:02 +0200

    Add solution for exercise 7

---
+++ src/main/java/com/camunda/academy/handler/PaymentCompletionHandler.java
@@ -0,0 +1,29 @@
+package com.camunda.academy.handler;
+
+import io.camunda.zeebe.client.ZeebeClient;
+import io.camunda.zeebe.client.api.response.ActivatedJob;
+import io.camunda.zeebe.client.api.worker.JobClient;
+import io.camunda.zeebe.spring.client.annotation.JobWorker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentCompletionHandler {
+
+  @Autowired
+  private ZeebeClient zeebeClient;
+
+  @JobWorker(type = "payment-completion", autoComplete = false)
+  public void handlePaymentCompletion(JobClient jobClient, ActivatedJob job) {
+    System.out.println("Job handled: " + job.getType());
+
+    // Tell order process to continue
+    zeebeClient.newPublishMessageCommand()
+      .messageName("paymentCompletedMessage")
+      .correlationKey(String.valueOf(job.getVariable("orderId")))
+      .send().join();
+
+    // Complete the job in the payment process
+    jobClient.newCompleteCommand(job.getKey()).send();
+  }
+}
---
+++ src/main/java/com/camunda/academy/handler/PaymentInvocationHandler.java
@@ -0,0 +1,37 @@
+package com.camunda.academy.handler;
+
+import io.camunda.zeebe.client.ZeebeClient;
+import io.camunda.zeebe.client.api.response.ActivatedJob;
+import io.camunda.zeebe.client.api.worker.JobClient;
+import io.camunda.zeebe.spring.client.annotation.JobWorker;
+import java.util.Map;
+import java.util.Random;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentInvocationHandler {
+
+  @Autowired
+  private ZeebeClient zeebeClient;
+
+  @JobWorker(type = "payment-invocation", autoComplete = false)
+  public void handlePaymentInvocation(JobClient jobClient, ActivatedJob job) {
+    System.out.println("Job handled: " + job.getType());
+
+    // Fetch variables from order process & generate random order ID
+    Map<String, Object> orderVariables = job.getVariablesAsMap();
+    int orderId = new Random().nextInt();
+    orderVariables.put("orderId", orderId);
+
+    // Start payment process
+    zeebeClient.newPublishMessageCommand()
+      .messageName("paymentRequestedMessage")
+      .withoutCorrelationKey()
+      .variables(orderVariables)
+      .send().join();
+
+    // Complete the job in the order process
+    jobClient.newCompleteCommand(job.getKey()).variable("orderId", orderId).send();
+  }
+}
