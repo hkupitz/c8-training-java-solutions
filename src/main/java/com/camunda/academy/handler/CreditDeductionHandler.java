3f2e039bda343c86d31c9fdc42c3e8b600cbe532
Hendrik Kupitz <hendrik.kupitz@camunda.com>
2024-05-06 12:22:03 +0200

    Remove redundant imports

--- src/main/java/com/camunda/academy/handler/CreditDeductionHandler.java
+++ src/main/java/com/camunda/academy/handler/CreditDeductionHandler.java
@@ -1,12 +1,8 @@
 package com.camunda.academy.handler;
 
-import com.camunda.academy.services.CustomerService;
 import io.camunda.zeebe.client.api.response.ActivatedJob;
 import io.camunda.zeebe.client.api.worker.JobClient;
 import io.camunda.zeebe.spring.client.annotation.JobWorker;
-import io.camunda.zeebe.spring.client.annotation.Variable;
-import java.util.Map;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
