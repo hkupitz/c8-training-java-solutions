827490ec80f21500ddef58803d947e7583506100
Hendrik Kupitz <hendrik.kupitz@camunda.com>
2024-12-06 15:23:54 +0100

    Update client version

--- src/main/java/com/camunda/academy/PaymentApplication.java
+++ src/main/java/com/camunda/academy/PaymentApplication.java
@@ -6,48 +6,52 @@ import io.camunda.zeebe.client.ZeebeClient;
 import io.camunda.zeebe.client.api.worker.JobWorker;
 import io.camunda.zeebe.client.impl.oauth.OAuthCredentialsProvider;
 import io.camunda.zeebe.client.impl.oauth.OAuthCredentialsProviderBuilder;
+
+import java.net.URI;
 import java.util.Scanner;
 
 public class PaymentApplication {
 
-  // Zeebe Client Credentials
-  private static final String ZEEBE_ADDRESS = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX.XXX-X.zeebe.camunda.io:443";
-  private static final String ZEEBE_CLIENT_ID = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-  private static final String ZEEBE_CLIENT_SECRET = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+  // Zeebe client credentials
+  private static final String ZEEBE_GRPC_ADDRESS = "grpcs://xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx.xxx-1.zeebe.camunda.io:443";
+  private static final String ZEEBE_REST_ADDRESS = "https://xxx-1.zeebe.camunda.io/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx";
+  private static final String ZEEBE_CLIENT_ID = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+  private static final String ZEEBE_CLIENT_SECRET = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
   private static final String ZEEBE_AUTHORIZATION_SERVER_URL = "https://login.cloud.camunda.io/oauth/token";
   private static final String ZEEBE_TOKEN_AUDIENCE = "zeebe.camunda.io";
 
   public static void main(String[] args) {
 
     final OAuthCredentialsProvider credentialsProvider = new OAuthCredentialsProviderBuilder()
-      .authorizationServerUrl(ZEEBE_AUTHORIZATION_SERVER_URL)
-      .audience(ZEEBE_TOKEN_AUDIENCE)
-      .clientId(ZEEBE_CLIENT_ID)
-      .clientSecret(ZEEBE_CLIENT_SECRET)
-      .build();
+            .authorizationServerUrl(ZEEBE_AUTHORIZATION_SERVER_URL)
+            .audience(ZEEBE_TOKEN_AUDIENCE)
+            .clientId(ZEEBE_CLIENT_ID)
+            .clientSecret(ZEEBE_CLIENT_SECRET)
+            .build();
 
     try (final ZeebeClient client = ZeebeClient.newClientBuilder()
-      .gatewayAddress(ZEEBE_ADDRESS)
-      .credentialsProvider(credentialsProvider)
-      .build()) {
+            .grpcAddress(URI.create(ZEEBE_GRPC_ADDRESS))
+            .restAddress(URI.create(ZEEBE_REST_ADDRESS))
+            .credentialsProvider(credentialsProvider)
+            .build()) {
 
-      // Request the Cluster Topology
+      // Request the cluster topology
       System.out.println("Connected to: " + client.newTopologyRequest().send().join());
 
-      // Start a Job Worker
+      // Start a job worker
       final JobWorker creditDeductionWorker = client.newWorker()
-        .jobType("credit-deduction")
-        .handler(new CreditDeductionHandler())
-        .open();
+              .jobType("credit-deduction")
+              .handler(new CreditDeductionHandler())
+              .open();
 
       final JobWorker creditCardChargingWorker = client.newWorker()
-        .jobType("credit-card-charging")
-        .handler(new CreditCardChargingHandler())
-        .open();
+              .jobType("credit-card-charging")
+              .handler(new CreditCardChargingHandler())
+              .open();
 
       // Terminate the worker with an Integer input
       Scanner sc = new Scanner(System.in);
-      sc.nextInt(); 
+      sc.nextInt();
       sc.close();
       creditCardChargingWorker.close();
 
